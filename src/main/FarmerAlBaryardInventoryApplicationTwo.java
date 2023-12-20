1e316b1bf61808d0b89a460cc66d2b00d8e1e486
Sonny Vanderhoeven <147276430+svander7@users.noreply.github.com>
2023-12-20 13:48:22 -0500

    First Attempt

--- src/main/FarmerAlBaryardInventoryApplicationTwo.java
+++ src/main/FarmerAlBaryardInventoryApplicationTwo.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Arrays;
+
 import main.animals.Pig;
 import main.service.FileService;
 
@@ -10,9 +12,23 @@ public class FarmerAlBaryardInventoryApplicationTwo {
 		FileService fileService = new FileService();
 		Pig[] pigs = fileService.getPigsFromFile();
 		String[] pigNames = new String[4];
+		
+		// Try displaying the array to the console, then sort it.
+		
+		pigNames[0] = pig;
+		pigNames[1] = pig1;
   		
 		// SORT the pigs by name...
 		
+		Arrays.sort(pigs, (pig1, pig2) -> pig1.getName().compareTo(pig2.getName()));
+		System.out.println("Sorted pig names:");
+		for (String pig : pigNames) {
+			System.out.println(pigNames);
+		}
+		
+//		for (String pig : pigNames) {
+//			System.out.println(pigNames);
+//		}
 		// 1. run a for loop on the pig array
 		// 2. In the for loop create an array called pig names, get the names of the pigs using
 		// the getName getter from the Pig class, save it into the pigNames string array
--- src/main/animals/Pig.java
+++ src/main/animals/Pig.java
@@ -1,17 +1,47 @@
 package main.animals;
 
 
-public class Pig {
+public class Pig implements Comparable<Pig> {
 
-	// CREATE the field "name" which is a String
-	// CREATE the field "status" which is a String
+	// CREATE the field "name" which is a String		= done
+	// CREATE the field "status" which is a String		= done
+	
+	private String name;
+	private String status;
 	
 	public Pig(String name, String status) {
-		// CREATE the constructor
+		// CREATE the constructor						= done
+		this.name = name;
+		this.status = status;
 	}
 	
 	
-	// Create getter and setter for name and status
+		// Create getter and setter for name and status	= done
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	@Override
+	public String compareTo(Pig that) {
+		if (this.name.compareTo(that.name) == 0) {
+			return this.name;  
+		} 
+	
+	
+	
 	
 	
 
